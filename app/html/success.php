77430865baf7b1ba8cb43d3b3fb2e3c76b9fc7bb
ChangMM <1769111903@qq.com>
2016-03-22 14:40:18 +0800

    添加引导关注

--- app/html/success.php
+++ app/html/success.php
@@ -22,6 +22,7 @@
        <p>
            如果你被选中，24小时之内会有工作人员与你联系，并安排约见，请耐心等待。
        </p>
+       <p class="warn-word">关注公众号【华科学习帝】，查看更多主题。</p>
    </div>
     <a href="share.php" class="detail-button">点击了解『取经』</a>
 </div>
