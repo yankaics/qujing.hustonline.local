c07f4800163204403b2e87a5585f65d5bab01980
ChangMM <1769111903@qq.com>
2016-03-23 16:16:33 +0800

    添加头像

--- app/html/util/mysql.php
+++ app/html/util/mysql.php
@@ -1,6 +1,7 @@
 <?php
     /***连接本地的数据库**/
-    mysql_connect("127.0.0.1","root","Hustonline87542701");
+    @mysql_connect("127.0.0.1","root","Hustonline87542701");
+    // mysqli_connect("127.0.0.1", 'root', 'Hustonline87542701', 'qujing');
     mysql_select_db("qujing");
     mysql_query("set names utf8");
 ?>
