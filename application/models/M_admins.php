6760f45228f59ace49deb32096b34b794b44d7b0
ChangMM <1769111903@qq.com>
2016-05-19 15:01:29 +0800

    修改模型文件名称

---
+++ application/models/M_admins.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cmm
+ * Date: 16/5/17
+ * Time: 下午10:36
+ */
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class M_admins extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function get($where = array())
+    {
+        $this->db->select('*');
+        $this->db->from('admins');
+        $this->db->where($where);
+
+        return $this->db->get()->row_array();
+    }
+}
+
+
